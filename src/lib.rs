72932aced972a9656debdaf1547834fcd4befd7e
Nathaniel Knight <nathaniel.ep@gmail.com>
2022-08-29 18:03:58 -0700

    More tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@
 //! [Functional Pearl: Enumerating the Rationals].
 //!
 //! [Functional Pearl: Enumerating the Rationals]: http://www.cs.ox.ac.uk/people/jeremy.gibbons/publications/rationals.pdf
-//!
-//!
 
 use num_integer::Integer;
 use num_rational::Ratio;
@@ -18,17 +16,23 @@ impl<T> Rationals<T>
 where
     T: Integer + Clone + FromPrimitive,
 {
+    /// Create a new iterator over the rationals.
+    ///
+    /// ```
+    /// use iter_rationals::Rationals;
+    ///
+    /// let mut rationals = Rationals::<u32>::new();
+    ///
+    /// for r in rationals.take(10) {
+    ///     println!("{r}");
+    /// }
+    /// ```
     pub fn new() -> Self {
-        Self {
-            state: Self::one(),
-        }
+        Self { state: Self::one() }
     }
 
     fn one() -> Ratio<T> {
-        Ratio::new(
-                T::from_u64(1).expect("Failed to convert '1'"),
-                T::from_u64(1).expect("Failed to convert from '1'")
-                )
+        Ratio::from_integer(T::from_u64(1).unwrap())
     }
 }
 
@@ -51,21 +55,55 @@ where
 #[test]
 fn test_speed_is_reasonable() {
     let mut r = Rationals::<u32>::new();
-    let millionth_rational = r.nth(1_000_000).unwrap();
+    let nth_rational = r.nth(1_000_000).unwrap();
+    println!("{nth_rational}");
     let expected = Ratio::new(1287, 1096);
-    assert_eq!(expected, millionth_rational);
+    assert_eq!(expected, nth_rational);
 }
 
 #[test]
 fn test_first_values_are_as_expected() {
     let expected_parts = [
         (1, 1),
-        (1, 2), (2, 1),
-        (1, 3), (3, 2), (2, 3), (3, 1),
-        (1, 4), (4, 3), (3, 5), (5, 2), (2, 5), (5, 3), (3, 4), (4, 1)
+        (1, 2),
+        (2, 1),
+        (1, 3),
+        (3, 2),
+        (2, 3),
+        (3, 1),
+        (1, 4),
+        (4, 3),
+        (3, 5),
+        (5, 2),
+        (2, 5),
+        (5, 3),
+        (3, 4),
+        (4, 1),
     ];
-    let expected: Vec<Ratio<u32>> = expected_parts.iter().map(|p| Ratio::new(p.0, p.1)).collect();
+    let expected: Vec<Ratio<u32>> = expected_parts
+        .iter()
+        .map(|p| Ratio::new(p.0, p.1))
+        .collect();
     let found: Vec<Ratio<u32>> = Rationals::<u32>::new().take(expected.len()).collect();
     assert_eq!(found, expected);
+}
 
+#[test]
+fn test_builtin_types() {
+    let limit = 32;
+
+    Rationals::<u8>::new().nth(limit).unwrap();
+    Rationals::<u16>::new().nth(limit).unwrap();
+    Rationals::<u32>::new().nth(limit).unwrap();
+    Rationals::<u64>::new().nth(limit).unwrap();
+    Rationals::<u128>::new().nth(limit).unwrap();
+
+    Rationals::<i8>::new().nth(limit).unwrap();
+    Rationals::<i16>::new().nth(limit).unwrap();
+    Rationals::<i32>::new().nth(limit).unwrap();
+    Rationals::<i64>::new().nth(limit).unwrap();
+    Rationals::<i128>::new().nth(limit).unwrap();
+
+    Rationals::<usize>::new().nth(limit).unwrap();
+    Rationals::<isize>::new().nth(limit).unwrap();
 }
